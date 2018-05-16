005a9075f3539465a6c61f294f9517bb2111fc15
Konrad Najder <najder.konrad@gmail.com>
2018-05-16 22:37:17 +0200

    Added working collision system, flash effect on collision, some css

--- sketch.js
+++ sketch.js
@@ -5,60 +5,80 @@ var veloMult;
 var circles = [];
 var velos = [];
 
+const pSize = 16;
+const defaultCollFrames = 3;
+
+let mult = p5.Vector.mult
+let sub = p5.Vector.sub
+let dot = p5.Vector.dot
+
+function range(from, to) {
+  return [...Array(to - from).keys()].map(n => n + from)
+}
+
 function Particle(x, y, vx, vy, ax, ay) {
-  if(x === undefined || y === undefined) {
+  if (x === undefined || y === undefined) {
     let [x, y] = [random(width), random(height)];
   }
-  if(vx === undefined || vy === undefined) {
+  if (vx === undefined || vy === undefined) {
     let [vx, vy] = [random(), random()];
   }
-  if(ax === undefined || ay === undefined) {
+  if (ax === undefined || ay === undefined) {
     let [ax, ay] = [random(), random()];
   }
 
   this.position = createVector(x, y);
   this.velocity = createVector(vx, vy);
   this.acceleration = createVector(ax, ay);
+  this.collisionFrames = 0;
 }
 
-Particle.prototype.draw = function() {
-  stroke(50);
-  fill(100);
-  ellipse(this.position.x, this.position.y, 24, 24);
+Particle.prototype.draw = function () {
+  if (this.collisionFrames > 0) {
+    stroke('red');
+    fill('pink');
+    ellipse(this.position.x, this.position.y, pSize, pSize); //just testin
+    this.collisionFrames -= 1;
+  } else {
+    stroke(50);
+    fill(100);
+    ellipse(this.position.x, this.position.y, pSize, pSize);
+  }
 }
 
-Particle.prototype.collision = function(particles) {
-  checkedParticle = this;
-  console.log(particles);
-  particles.forEach(function(particle) {
-    
-    let distance = checkedParticle.position.dist(particle.position)
+Particle.prototype.collision = function (other) {
+  let distance = this.position.dist(other.position)
+  if (distance <= pSize) {
+    let v1 = this.velocity;
+    let v2 = other.velocity;
+    let x1mx2 = sub(this.position, other.position);
+    let x2mx1 = sub(other.position, this.position);
 
-    if(distance <= 24) {
-      checkedParticle.velocity.x *= -1;
-      checkedParticle.velocity.y *= -1;
-    }   
-  });
-  
+    this.velocity = sub(v1, mult(x1mx2, dot(sub(v1, v2), x1mx2) / dot(x1mx2, x1mx2)));
+    other.velocity = sub(v2, mult(x2mx1, dot(sub(v2, v1), x2mx1) / dot(x2mx1, x2mx1)));
+
+    this.collisionFrames = defaultCollFrames;
+    other.collisionFrames = defaultCollFrames;
+  }
 }
 
-Particle.prototype.updateState = function() {
+Particle.prototype.updateState = function () {
   this.position.add(this.velocity);
   this.velocity.add(this.acceleration);
 
-  if(this.position.x < 0 || this.position.x > width) {
+  if ((this.position.x - pSize / 2) < 0 || (this.position.x + pSize / 2) >= width) {
     this.velocity.x *= -1;
   }
-  if(this.position.y < 0 || this.position.y > width) {
+  if ((this.position.y - pSize / 2) < 0 || (this.position.y + pSize / 2) > height) {
     this.velocity.y *= -1;
   }
 }
 
 function initCircles() {
-  for(var i = 0; i < numOfCircles.value(); i++) {
+  for (var i = 0; i < numOfCircles.value(); i++) {
     circles[i] = new Particle(
       random(width), random(height),
-      random(-1*veloMult.value(), veloMult.value()), random(-1*veloMult.value(), veloMult.value()),
+      random(-1 * veloMult.value(), veloMult.value()), random(-1 * veloMult.value(), veloMult.value()),
       0, 0
     )
     //circles[i] = createVector(random(width), random(height));
@@ -71,9 +91,9 @@ function initCircles() {
 function setup() {
   createCanvas(1080, 720);
   createP('');
-  numOfCircles = createSlider(0, 2000, 30);
+  numOfCircles = createSlider(0, 500, 150);
   numOfCircles.changed(initCircles);
-  veloMult = createSlider(1, 10, 1);
+  veloMult = createSlider(1, 10, 4);
   veloMult.changed(initCircles);
   initCircles();
   textSize(32);
@@ -86,24 +106,10 @@ function draw() {
   noStroke();
   text(`simple ${lastValue} particles test`, 10, height - 10);
 
-  for(var i = 0; i < lastValue; i++) {
-    //stroke(50);
-    //fill(100);
-    //ellipse(circles[i].position.x, circles[i].position.y, 24, 24);
+  range(0, lastValue).forEach(i => {
     circles[i].draw();
-    circles[i].updateState()
-    circles.forEach(function(circle) {
-      //console.log(circle)
-      circle.collision(circles);
-    });
-    //circles[i].add(velos[i]);
-    // circles[i].updateState()
-    // if(circles[i].x < 0 || circles[i].x > width) {
-    //   velos[i].x = velos[i].x * -1;
-    // }
-
-    // if(circles[i].y < 0 || circles[i].y > height) {
-    //   velos[i].y = velos[i].y * -1;
-    // }
-  }
+    circles[i].updateState();
+    range(i + 1, lastValue).forEach(j => circles[i].collision(circles[j]));
+  });
 }
+
